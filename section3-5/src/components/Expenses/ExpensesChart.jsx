6d67e106f0ae8cefc16b8bff5e1a5e1af2414b5a
Arem58 <arem.9958@gmail.com>
2022-08-24 18:33:40 -0600

    correccion de error

--- section3-5/src/components/Expenses/ExpensesChart.jsx
+++ section3-5/src/components/Expenses/ExpensesChart.jsx
@@ -4,18 +4,18 @@ import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
   const chartDataPoints = [
-    { laber: "Jan", value: 0 },
-    { laber: "Feb", value: 0 },
-    { laber: "Mar", value: 0 },
-    { laber: "Apr", value: 0 },
-    { laber: "May", value: 0 },
-    { laber: "Jun", value: 0 },
-    { laber: "Jul", value: 0 },
-    { laber: "Aug", value: 0 },
-    { laber: "Sep", value: 0 },
-    { laber: "Oct", value: 0 },
-    { laber: "Nov", value: 0 },
-    { laber: "Dec", value: 0 },
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
   ];
 
   for (const expense of props.expenses) {
